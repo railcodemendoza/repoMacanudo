8877358cafe867d595eba88ed5f0d32a4ba9766a
JuanIgnacioOlivares <juaniolivares95@gmail.com>
2022-05-31 15:59:07 -0300

    Agregados y testeados

--- dashboard/db.php
+++ dashboard/db.php
@@ -1,9 +1,9 @@
 <?php
 $conn = mysqli_connect(
   '31.170.161.22',//31.170.161.22
-  'u101685278_labmac',
-  'Rail2021',
-  'u101685278_labmac' 
+  'u101685278_pachimanok', // u101685278_labmac
+  'Pachiman9102', // Rail2021
+  'u101685278_macanudas' // u101685278_labmac
 ) or die(mysqli_erro($mysqli));
 
 ?>
